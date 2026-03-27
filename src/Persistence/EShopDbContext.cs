42fb2b7bc5c4f84dc5eb5b2255de2ece5794bb05
Md. Abdul Baset Shahid <abdul.baset@niftycoders.com>
2026-03-27 20:51:59 +0600

    Add Db Context

---
+++ src/Persistence/EShopDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class EShopDbContext : DbContext
+    {
+        public EShopDbContext(DbContextOptions<EShopDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
